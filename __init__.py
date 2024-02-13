95791021f4ad66282a3a13ed67b93f102fba4740
StanislasB <33927838+StanislasB@users.noreply.github.com>
2024-02-13 14:23:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Page de Contact du S</h2>"
+  
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -20,6 +21,10 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
                                                                                                                                        
 @app.route('/')
 def hello_world():
